554c7d75a7a76e5b42f7d06dc39a67b4b4996f80
Zedd0202 <ch41150@hanmail.net>
2020-06-17 14:04:58 +0900

    some change

--- PRActions/ViewController.swift
+++ PRActions/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("dd")
         // Do any additional setup after loading the view.
     }
+    
+    
 }
 
